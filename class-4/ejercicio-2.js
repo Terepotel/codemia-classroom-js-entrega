4565fb5b29e57dbf2e117d98260a5082e90d2db4
tere <terepotel13@gmail.com>
2022-07-28 20:50:21 -0300

    actividad 2 de la clase 4 hay 2 soluciones

---
+++ class-4/ejercicio-2.js
@@ -0,0 +1,18 @@
+let termino = 25;
+let ultimoPaso =11 * termino
+let numb = 0;
+    do{
+        numb = numb + 11;
+        console.log (numb);
+    }
+    while (numb < ultimoPaso)
+
+/*segunda solucion
+let numb = 0;
+let termino = 1;
+    do{
+        numb = numb + 11;
+        termino = termino + 1;
+        console.log (numb);
+    }
+    while (termino <= 25)
