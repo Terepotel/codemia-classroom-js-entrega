4fd8ffff349784ad1a79d0681dd2b571331ce48b
tere <terepotel13@gmail.com>
2022-07-28 20:37:50 -0300

    ejercicio 1 clase 4 de Js ejercicio 1 clase 4 de Js.  modified:   class-4/ejercicio-1.js

---
+++ class-4/ejercicio-1.js
@@ -0,0 +1,20 @@
+function traducir (palabra) {
+    switch (palabra){
+        case "perro":
+            console.log("dog");
+            break;
+        case "casa":
+            console.log("house");
+            break;
+        case "mesa":
+            console.log("table");
+            break;
+        case "gato":
+            console.log("cat")
+            break;
+        default:
+            console.log(" ");
+    }
+    
+}
+traducir("perro")
