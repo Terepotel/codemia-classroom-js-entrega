7218cb6fdd2aaf41eef2ac8a4f24ecfa1f4db1a6
tere <terepotel13@gmail.com>
2022-08-22 11:41:27 -0300

    subir ejercicio 1 clase 2

---
+++ class-2/ejercicio-1.js
@@ -0,0 +1,24 @@
+function horasdDelDiaTernario(hora) {
+    console.log(hora >= 21
+      ? "Buenas Noches"
+      : hora >= 19
+      ? "Buenas tardes"
+      : hora >= 14
+      ? "Buenos medio dia"
+      : "Buenos dias";
+    )
+  }
+  horasdDelDiaTernario(22);
+  
+  /*
+  function horasdDelDiaTernario(hora) {
+    if(hora>=21){
+       console.log("Buenas Noches")
+   } else if (hora>=19){
+       console.log("Buenos medios dias")
+   }else if(hora>=9){
+       console.log("Buenos dias")
+   }
+   horasDelDiaTernario(22);
+   */
+  
